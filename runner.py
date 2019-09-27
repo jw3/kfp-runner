1fdc31391240e238c8385d414c18f18da4a838f2
John Wass <wassj@ctc.com>
2019-09-27 00:28:13 -0400

    fix experiment handling

--- runner.py
+++ runner.py
@@ -4,14 +4,15 @@ import kfp
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("job_name", type=str, help="Name for job")
-    parser.add_argument("-e", "--experiment", type=str, default='testing', help="Experiment name")
-    parser.add_argument("-p", "--pipeline", type=str, required=True, env_var='KF_PIPELINE_ID', help="Pipeline ID")
+    parser.add_argument("-e", "--experiment_name", type=str, default='testing', help="Experiment name")
+    parser.add_argument("-p", "--pipeline_id", type=str, required=True, env_var='KF_PIPELINE_ID', help="Pipeline ID")
     args = parser.parse_args()
 
     client = kfp.Client()
-    eid = client.get_experiment(experiment_name=args.experiment)
 
-    if not eid:
-        eid = client.create_experiment(args.experiment)
+    try:
+        e = client.get_experiment(experiment_name=args.experiment_name)
+    except ValueError:
+        e = client.create_experiment(args.experiment_name)
 
-    client.run_pipeline(eid, args.job_name, pipeline_id=args.pipeline)
+    client.run_pipeline(e.id, args.job_name, pipeline_id=args.pipeline_id)
